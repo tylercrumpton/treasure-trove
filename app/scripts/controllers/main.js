5f566ccefb6a0840bdf8c851c0c80d850744307c
Tyler <tyler.crumpton@gmail.com>
2015-04-28 23:46:10 -0500

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name treasuretroveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the treasuretroveApp
+ */
+angular.module('treasuretroveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
