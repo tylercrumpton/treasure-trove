bdeadf175c9c31e1cd543df17e19fb356720e842
Tyler <tyler.crumpton@gmail.com>
2015-04-30 00:18:36 -0500

    Clicking buttons now toggles state.

--- app/scripts/controllers/table.js
+++ app/scripts/controllers/table.js
@@ -6,9 +6,15 @@ angular.module('treasuretroveApp')
     $scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.changeState = function (user, month) {
       console.log('user: %d, month: %d', user, month);
-      $scope.messages.push("Message");
+      $scope.messages.push('Message');
+      var prev = $scope.membersDict[user].months[month];
+      $scope.membersDict[user].months[month] = 1-prev;
+      
     };
-    var arrayDefault = Array.apply(null, Array(24)).map(function() { return 0 });
-    $scope.membersDict = {'Tyler':arrayDefault, 'Nathan':arrayDefault, 'Hunter':arrayDefault};
+    $scope.membersDict = [
+      { name: 'Tyler', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
+      { name: 'Nathan', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
+      { name: 'Hunter', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
+    ];
     $scope.messages = [];
   });
