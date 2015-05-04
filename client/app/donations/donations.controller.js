177e1da2cf4b55c972566833496dcf9683b4d704
Tyler <tyler.crumpton@gmail.com>
2015-05-03 23:49:46 -0500

    Clean up some cruft.

--- client/app/donations/donations.controller.js
+++ client/app/donations/donations.controller.js
@@ -2,26 +2,15 @@
 
 angular.module('testmeanApp')
   .controller('DonationsCtrl', function ($scope, $http, socket) {
-    // TODO: Get the users from LDAP:
-    $scope.members = ['User 1', 'User 2', 'User 3'];
-
     var date = new Date();
     $scope.year = date.getFullYear();
     $scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    $scope.changeState = function (user, month) {
-      console.log('user: %d, month: %d', user, month);
-      $scope.messages.push('Message');
-      var prev = $scope.membersDict[user].months[month];
-      $scope.membersDict[user].months[month] = 1-prev;
-
-    };
+    // TODO: Get the users from LDAP and paid months from DB:
     $scope.membersDict = [
       { name: 'Tyler', username: 'tylercrumpton', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
       { name: 'Nathan', username: 'itsamenathan', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
       { name: 'Hunter', username: 'hunter', months: [0,0,0,0,0,0,0,0,0,0,0,0] },
     ];
-    $scope.messages = [];
-
     $http.get('/api/payments').success(function(memberPayments) {
       $scope.memberPayments = memberPayments;
       socket.syncUpdates('payment', $scope.memberPayments);
