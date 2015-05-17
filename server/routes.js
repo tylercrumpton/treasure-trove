8c8b0c657d6c41e30687838b52a14b5516545106
Tyler <tyler.crumpton@gmail.com>
2015-05-16 22:19:51 -0500

    Remove more unused scaffolding.

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/payments', require('./api/payment'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
