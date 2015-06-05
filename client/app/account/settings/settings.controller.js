f3069298dbe41242542d8512fbfc67ee36f10138
Tyler <tyler.crumpton@gmail.com>
2015-06-04 19:26:16 -0500

    Make the user settings page a bit more useful.

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -2,20 +2,5 @@
 
 angular.module('testmeanApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
-    $scope.errors = {};
-
-    $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
-      }
-		};
+    $scope.getCurrentUser = Auth.getCurrentUser;
   });
