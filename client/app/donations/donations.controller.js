6ef23d34654b37dd5d5e774fd331c188a65e05a5
Tyler <tyler.crumpton@gmail.com>
2015-06-07 23:18:38 -0500

    Fix sorted table toggle error.

--- client/app/donations/donations.controller.js
+++ client/app/donations/donations.controller.js
@@ -8,47 +8,47 @@ angular.module('testmeanApp')
     $scope.sortType     = 'realname'; // set the default sort type
     $scope.sortReverse  = false;  // set the default sort order
     $scope.searchName   = '';     // set the default search/filter term    
-    var processUserPayments = function(user) {
-      $http.get('/treasure/api/payments/users/'+$scope.membersDict[user].username+'/'+$scope.year).success(function(memberPayments) {
+    var processUserPayments = function(username) {
+      $http.get('/treasure/api/payments/users/'+username+'/'+$scope.year).success(function(memberPayments) {
         for (var j=0; j<memberPayments.length; j++) {
           var month = memberPayments[j].month;
-          $scope.membersDict[user].months[month] = 1;
+          $scope.membersDict[username].months[month] = 1;
         }
       });
     };
    
-    $scope.membersDict = []; 
+    $scope.membersDict = {}; 
     User.query().$promise.then(function(result){
       angular.forEach(result, function(u, i) {
-        $scope.membersDict[i] = {username: u.username, realname: u.realname, email: u.email, months: [0,0,0,0,0,0,0,0,0,0,0,0]};
-        processUserPayments(i);
+        $scope.membersDict[u.username] = {username: u.username, realname: u.realname, email: u.email, months: [0,0,0,0,0,0,0,0,0,0,0,0]};
+        processUserPayments(u.username);
       });
     });
 
-    $scope.togglePayment = function(user, month) {
-      if($scope.membersDict[user].months[month] == 0) {
-        $http.post('/treasure/api/payments', { name: $scope.membersDict[user].username,
+    $scope.togglePayment = function(username, month) {
+      if($scope.membersDict[username].months[month] == 0) {
+        $http.post('/treasure/api/payments', { name: username,
                                       month: month,
                                       year: $scope.year });
       } else {
-        $http.delete('/treasure/api/payments/users/'+$scope.membersDict[user].username+'/'+$scope.year+'/'+month);
+        $http.delete('/treasure/api/payments/users/'+username+'/'+$scope.year+'/'+month);
       }
 
-      var prev = $scope.membersDict[user].months[month];
-      $scope.membersDict[user].months[month] = 1-prev;
+      var prev = $scope.membersDict[username].months[month];
+      $scope.membersDict[username].months[month] = 1-prev;
 
     };
 
     socket.socket.on('payment:save', function (data) {
       for (var i=0; i<$scope.membersDict.length; i++) {
-        if ($scope.membersDict[i].username == data.name) {
+        if ($scope.membersDict[i] == data.name) {
           $scope.membersDict[i].months[data.month] = 1;
         }
       }
     });
     socket.socket.on('payment:remove', function (data) {
       for (var i=0; i<$scope.membersDict.length; i++) {
-        if ($scope.membersDict[i].username == data.name) {
+        if ($scope.membersDict[i] == data.name) {
           $scope.membersDict[i].months[data.month] = 0;
         }
       }
