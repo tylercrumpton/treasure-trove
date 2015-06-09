1c67f4f60a7ec395f0c9e8fd7cd2f2898a212940
Tyler <tyler.crumpton@gmail.com>
2015-06-08 22:34:19 -0500

    Add filter/search mechanism to donations table.

--- client/app/donations/donations.controller.js
+++ client/app/donations/donations.controller.js
@@ -15,8 +15,8 @@ angular.module('testmeanApp')
     $scope.year = date.getFullYear();
     $scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.sortType     = 'realname'; // set the default sort type
-    $scope.sortReverse  = false;  // set the default sort order
-    $scope.searchName   = '';     // set the default search/filter term    
+    $scope.sortReverse  = false; // set the default sort order
+    $scope.searchMembers   = ''; // set the default search/filter term
     var processUserPayments = function(username) {
       $http.get('/treasure/api/payments/users/'+username+'/'+$scope.year).success(function(memberPayments) {
         for (var j=0; j<memberPayments.length; j++) {
