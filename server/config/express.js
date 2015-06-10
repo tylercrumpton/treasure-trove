f799db4a0a8b56ee75e5674918b9e5ca47475acd
Tyler <tyler.crumpton@gmail.com>
2015-06-09 23:54:22 -0500

    Fix livereload route.

--- server/config/express.js
+++ server/config/express.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
 
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')({
-      src: "https://crump.space:35729/treasure/livereload.js?snipver=1"
+      src: "https://crump.space/treasure-live/livereload.js?snipver=1&host=crump.space:443/treasure-live/"
     }));
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
