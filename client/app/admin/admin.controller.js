8bc4833002085d803b2ba662ede85921b407a352
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-07-06 23:53:01 -0500

    Add export-json button.

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -6,6 +6,15 @@ angular.module('testmeanApp')
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
+    $scope.downloadJson = function() {
+      $http.get('/treasure/api/payments/export/json').success(function(data) {
+        var jsondata = JSON.stringify(data, null, '\t');
+        var blob = new Blob([jsondata], {type: "text/plain;charset=utf-8"});
+        var filename = 'ttexport_' + moment().format('YYYY-MM-DD-HHMMSS') + '.json';
+        saveAs(blob, filename);
+      });
+    };
+
     $scope.delete = function(user) {
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
